3019b3819043e07d5704e9f07ddfa746b62f77d0
misterio <misterio92@gmail.com>
2017-03-13 01:15:27 +0300

    Tests: ChannelManager

---
+++ tests/Unit/Channels/ChannelManagerTest.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit\Channels;
+
+use FondBot\Channels\ChannelManager;
+use FondBot\Channels\Drivers\Telegram;
+use FondBot\Database\Entities\Channel;
+use Tests\TestCase;
+
+/**
+ * @property ChannelManager manager
+ */
+class ChannelManagerTest extends TestCase
+{
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->manager = new ChannelManager;
+    }
+
+    public function test_driver()
+    {
+        $channel = new Channel([
+            'driver' => Telegram::class,
+            'name' => $this->faker()->name,
+            'parameters' => ['token' => str_random()],
+        ]);
+
+        $driver = $this->manager->driver(request(), $channel, true);
+
+        $this->assertInstanceOf(Telegram::class, $driver);
+    }
+
+    public function test_supportedDrivers()
+    {
+        $expected = [
+            'Telegram' => Telegram::class,
+        ];
+
+        $this->assertEquals($expected, $this->manager->supportedDrivers());
+    }
+
+}
