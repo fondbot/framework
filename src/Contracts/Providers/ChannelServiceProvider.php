5e841cc03354851320ea5f4559f13af3c7250744
Vladimir Yuldashev <misterio92@gmail.com>
2017-03-23 14:18:39 +0300

    Move ServiceProviders to Contracts

--- src/Contracts/Providers/ChannelServiceProvider.php
+++ src/Contracts/Providers/ChannelServiceProvider.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace FondBot\Contracts\Providers;
 
-use FondBot\Channels\ChannelManager;
 use FondBot\Channels\Facebook;
 use FondBot\Channels\Telegram;
 use FondBot\Channels\VkCommunity;
+use FondBot\Channels\ChannelManager;
 use FondBot\Providers\ServiceProvider;
 
 class ChannelServiceProvider extends ServiceProvider
