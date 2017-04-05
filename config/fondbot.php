e723178f2c80d836e90620a31121efa6d3d20d17
Vladimir Yuldashev <misterio92@gmail.com>
2017-04-05 13:22:28 +0300

    Configuration updated

--- config/fondbot.php
+++ config/fondbot.php
@@ -2,25 +2,47 @@
 
 return [
 
+    /**
+     * FondBot has no built-in drivers.
+     * However, there are official and third-party drivers you may plug-in.
+     * Here you need to define an alias and driver class
+     */
+    'drivers' => [
+        // 'telegram' => FondBot\Drivers\Telegram\TelegramDriver::class,
+        // 'facebook' => FondBot\Drivers\Facebook\FacebookDriver::class,
+        // 'vk-communities' => FondBot\Drivers\VkCommunity\VkCommunityDriver::class,
+    ],
+
     /*
-     * Define all channels for your bot here.
+     * Here you specify channels which FondBot will use when receiving webhooks.
+     * Read more about configuring: https://docs.fondbot.com/#/configuration?id=channels
      */
     'channels' => [
-
+        // 'telegram-1' => [
+        //      'driver' => 'telegram',
+        //      'token' => 'token-for-telegram-1',
+        // ],
+        // 'facebook-1' => [
+        //      'driver' => 'facebook',
+        //      'page_token' => '',
+        //      'verify_token' => '',
+        //      'app_secret' => '',
+        // ],
+        // 'vk-communities-1' => [
+        //      'driver' => 'vk-communities',
+        //      'access_token' => '',
+        //      'confirmation_token' => '',
+        // ],
     ],
 
     /*
-     * Here you define all intents which be used.
-     *
-     * Example: App\WeatherIntent::class
+     * Here you define all intents.
      */
     'intents' => [
-
+        // App\WeatherIntent::class
     ],
 
     /*
-     * Define fallback intent.
-     *
      * If no intent found based on your configuration this intent will be run.
      * You can send some helpful information in it.
      */
