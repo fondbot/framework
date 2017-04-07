c2119b86430352f0d9683ea46a3fa4c4f6901842
Vladimir Yuldashev <misterio92@gmail.com>
2017-04-07 12:16:00 +0300

    Channel get single parameter value method

--- src/Channels/Channel.php
+++ src/Channels/Channel.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace FondBot\Channels;
 
+use FondBot\Helpers\Arr;
+
 class Channel
 {
     private $name;
@@ -45,5 +47,18 @@ class Channel
     public function getParameters(): array
     {
         return $this->parameters;
+    }
+
+    /**
+     * Get single parameter value.
+     *
+     * @param string $name
+     * @param null   $default
+     *
+     * @return mixed
+     */
+    public function getParameter(string $name, $default = null)
+    {
+        return Arr::get($this->parameters, $name, $default);
     }
 }
