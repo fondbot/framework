5ee582bf86627d6f638c3b6c05fb26fad19ab72f
Vladimir Yuldashev <misterio92@gmail.com>
2017-05-29 12:07:40 +0300

    all buttons must implement setLabel and getLabel methods

--- src/Templates/Keyboard/Button.php
+++ src/Templates/Keyboard/Button.php
@@ -8,4 +8,19 @@ use FondBot\Contracts\Template;
 
 interface Button extends Template
 {
+    /**
+     * Get label.
+     *
+     * @return string
+     */
+    public function getLabel(): string;
+
+    /**
+     * Set label.
+     *
+     * @param string $label
+     *
+     * @return static
+     */
+    public function setLabel(string $label);
 }
