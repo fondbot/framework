9664dc645cd55759c7d4501e3a9e6f3d4f8910ea
Vladimir Yuldashev <misterio92@gmail.com>
2017-07-10 21:18:16 +0300

    Update Kernel version

--- src/Application/Kernel.php
+++ src/Application/Kernel.php
@@ -12,7 +12,7 @@ use FondBot\Conversation\SessionManager;
 
 class Kernel
 {
-    public const VERSION = '1.0.5';
+    public const VERSION = '1.1.0';
 
     /** @var Kernel */
     private static $instance;
