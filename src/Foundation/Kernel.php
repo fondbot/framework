c010a4bff49554ab0fa0eb2e1910f42ddc495777
vyuldashev <misterio92@gmail.com>
2017-09-12 00:07:13 +0300

    wip

--- src/Foundation/Kernel.php
+++ src/Foundation/Kernel.php
@@ -5,21 +5,13 @@ declare(strict_types=1);
 namespace FondBot\Foundation;
 
 use FondBot\Channels\Channel;
-use Illuminate\Contracts\Container\Container;
 
 class Kernel
 {
     public const VERSION = '2.0';
 
-    private $container;
-
     /** @var Channel|null */
     private $channel;
-
-    public function __construct(Container $container)
-    {
-        $this->container = $container;
-    }
 
     /**
      * Initialize kernel.
--- src/Foundation/Providers/FoundationServiceProvider.php
+++ src/Foundation/Providers/FoundationServiceProvider.php
@@ -6,15 +6,13 @@ namespace FondBot\Foundation\Providers;
 
 use FondBot\Foundation\Kernel;
 use FondBot\Foundation\ServiceProvider;
-use Illuminate\Contracts\Bus\Dispatcher;
-use Illuminate\Contracts\Container\Container;
 
 class FoundationServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         $this->app->singleton(Kernel::class, function () {
-            return new Kernel(resolve(Container::class), resolve(Dispatcher::class));
+            return new Kernel;
         });
     }
 }
