0ef365716b719034c99b1415d72dbc17fe14ef23
vyuldashev <misterio92@gmail.com>
2017-11-07 18:40:04 +0300

    wip

--- src/Foundation/ServiceProvider.php
+++ src/Foundation/ServiceProvider.php
@@ -8,10 +8,12 @@ use FondBot\Toolbelt\ToolbeltServiceProvider;
 use Illuminate\Support\AggregateServiceProvider;
 use FondBot\Foundation\Providers\RouteServiceProvider;
 use FondBot\Foundation\Providers\FoundationServiceProvider;
+use FondBot\Framework\Providers\ConsoleSupportServiceProvider;
 
 class ServiceProvider extends AggregateServiceProvider
 {
     protected $providers = [
+        ConsoleSupportServiceProvider::class,
         FoundationServiceProvider::class,
         ToolbeltServiceProvider::class,
         RouteServiceProvider::class,
